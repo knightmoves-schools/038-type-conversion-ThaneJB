cd4d03eca4b99c07d198f3ecc1bc72e401d28577
Thane <thabay0606@students.maquoketaschools.org>
2026-04-13 19:20:37 +0000

    Maybe.

--- Converter.cs
+++ Converter.cs
@@ -2,7 +2,14 @@
 
 public class Converter{
    //Add your code here
-   public double ConvertImplicitly(){
-      int intValue = "";
+   public double ConvertImplicitly(int intValue){
+      double doubleValue = intValue;
+      return doubleValue;
+   }
+
+   public int ConvertExplicitly(double doubleValue2)
+   {
+      int intValue2 = (int)doubleValue2;
+      return intValue2;
    }
 }
