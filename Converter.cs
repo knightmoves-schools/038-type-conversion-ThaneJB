d8ed6c4b7f493dabbee2fd1ad3619768620542e8
Thane <thabay0606@students.maquoketaschools.org>
2026-04-13 19:30:41 +0000

    Maybe now.

--- Converter.cs
+++ Converter.cs
@@ -2,23 +2,14 @@
 
 public class Converter{
    //Add your code here
-   public double ConvertImplicitly(int intValue){
-      double doubleValue = intValue;
-      return doubleValue;
+   public decimal ConvertImplicitly(int intValue){
+      decimal decimalValue = intValue;
+      return decimalValue;
    }
 
-   public int ConvertExplicitly(double doubleValue2)
+   public int ConvertExplicitly(decimal decimalValue2)
    {
-      int intValue2 = (int)doubleValue2;
+      int intValue2 = (int)decimalValue2;
       return intValue2;
-   }
-   public string ConvertToString(decimal value1, double value2)
-   {
-      return $"{value1} {value2}";
-   }
-
-   public double ConvertDecimalToDouble(decimal value)
-   {
-      return (double)value;
    }
 }
