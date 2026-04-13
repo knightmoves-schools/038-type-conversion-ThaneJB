8103a9f1feb895390cf87fc38907828480765f2e
Thane <thabay0606@students.maquoketaschools.org>
2026-04-13 19:24:23 +0000

    Had help from GPT

--- Converter.cs
+++ Converter.cs
@@ -12,4 +12,13 @@ public class Converter{
       int intValue2 = (int)doubleValue2;
       return intValue2;
    }
+   public string ConvertToString(decimal value1, double value2)
+   {
+      return $"{value1} {value2}";
+   }
+
+   public double ConvertDecimalToDouble(decimal value)
+   {
+      return (double)value;
+   }
 }
